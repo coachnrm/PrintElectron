e6e63058b5324d4cbb6dcf0b161ec38a78dcea9e
coachnrm <nuttapong_coach@hotmail.com>
2024-12-20 11:27:41 +0700

    add code

--- preload.js
+++ preload.js
@@ -7,7 +7,21 @@ contextBridge.exposeInMainWorld('electronAPI', {
   fetchWardData: async () => {
     try {
       // Make a GET request to the API
-      const response = await fetch('http://localhost:5094/api/Ipd/getward');
+      const response = await fetch('http://172.16.200.202:8089/api/Ipd/getward');
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return await response.json(); // Parse JSON response
+    } catch (error) {
+      console.error('Error fetching ward data:', error);
+      throw error; // Re-throw error to handle it in the renderer process
+    }
+  },
+
+  fetchAuthenData: async () => {
+    try {
+      // Make a GET request to the API
+      const response = await fetch('http://localhost:8189/api/smartcard/read?readImageFlag=false');
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
